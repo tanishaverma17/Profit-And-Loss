6d3152a126b68779152025eca7c2b8bff0fbbd94
tanishaverma17 <76125836+tanishaverma17@users.noreply.github.com>
2023-07-16 16:53:47 +0530

    add- CSS
    added styling

--- app.js
+++ app.js
@@ -6,17 +6,20 @@ var output = document.querySelector("#output-box");
 
 function CalculateProfitAndLoss(initial, quantity,current){
     if(initial >current){
+        output.setAttribute("class", "error");
         var loss = (initial - current)*quantity;
         var lossPercentage = (loss/initial)*100;
         output.innerText = "The loss is" + loss + "and the lossPercentage is" + lossPercentage +"%";
     }
     else if(initial < current){
+        output.setAttribute("class", "okay");
         var profit = (current - initial)*quantity;
         var profitPercentage = (profit/initial)*100;
         output.innerText = "The profit is" + profit +"and the profitPercentage is" + profitPercentage +"%";
     }
     else{
-        output.innerText = "No Pain No Gain, No Gain No Pain";
+        output.setAttribute("class", "error");
+        output.innerText = "Enter Valid values❗";
     }
 }
 
