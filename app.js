37572a547c595498a3957668955c56fb557895a4
tanishaverma17 <76125836+tanishaverma17@users.noreply.github.com>
2023-07-18 21:53:42 +0530

    corrections made
    in css

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ function CalculateProfitAndLoss(initial, quantity,current){
         output.setAttribute("class", "error");
         var loss = (initial - current)*quantity;
         var lossPercentage = (loss/initial)*100;
-        output.innerText = "The loss is" + loss + "and the lossPercentage is" + lossPercentage +"%";
+        output.innerText = "The loss is " + loss + "and the lossPercentage is " + lossPercentage +"%";
     }
     else if(initial < current){
         output.setAttribute("class", "okay");
         var profit = (current - initial)*quantity;
         var profitPercentage = (profit/initial)*100;
-        output.innerText = "The profit is" + profit +"and the profitPercentage is" + profitPercentage +"%";
+        output.innerText = "The profit is " + profit +"and the profitPercentage is " + profitPercentage +"%";
     }
     else{
         output.setAttribute("class", "error");
